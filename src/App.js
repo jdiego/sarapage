dc155c82117a307350529ff60f781b34184ca812
Diego Saraiva <diegosaraiva@gmai.com>
2020-09-26 17:06:23 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="">
-        <div className="">
-          Blessing Krofegha
-        </div>
-        <p className="">
-            Ei deu certo?
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-      <div className="">
-        <span className="">#Software Engineer</span>
-        <span className="">#Writter</span>
-        <span className="">#Public Speaker</span>
-      </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
