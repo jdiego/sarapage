c079b7767d2050373e9c1e8b0520e8d94c525d8e
Diego Saraiva <diegosaraiva@gmail.com>
2020-09-27 00:39:53 -0300

    testing

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Container, Row, Col, Section } from '@bootstrap-styled/v4';
 import './styles.css';
-
 function Hero(){
     return (
-        <div className="hero">
-            <div className="hero-title">
-                <h1>Taro</h1>
-            </div>
-        </div>
+        <section id="title">
+            <Container className="bg-primary" fluid={true}>
+                <Row>
+                    <Col lg="6" className="mx-auto">
+                        <h1 className="title">Sara Melo</h1>
+                        <h2> Taro sistêmcia</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
-import { Container } from '@bootstrap-styled/v4';
 import BootstrapProvider from '@bootstrap-styled/provider/lib/BootstrapProvider';
-
 // import sections
 import Hero from '../../components/Hero';
 class Home extends Component {
     render() {
       return (
         <BootstrapProvider>
-          <Container className="py-2">
-            <div className="bg-primary">Header</div>
-            <div className="w-25 bg-warning d-inline-block">Sidebar</div>
-            <div className="w-75 bg-info d-inline-block">Body</div>
-          </Container>
+          <Hero></Hero>
         </BootstrapProvider>
       );
     }
