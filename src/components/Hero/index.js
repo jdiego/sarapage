09fe2d563e7a9a12a1a16f1b0776e70422aaf7e5
Diego Saraiva <diegosaraiva@gmail.com>
2020-09-27 00:15:46 -0300

    testing bootstrap components

---
+++ src/components/Hero/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function Hero(){
+    return (
+        <div className="hero">
+            <div className="hero-title">
+                <h1>Taro</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
