b00deac2d2022b8ad667c6223a194b2b89a1cc53
Diego Saraiva <diegosaraiva@gmai.com>
2020-09-29 20:03:55 -0300

    Updates

---
+++ precache-manifest.a2af34c8593c8548c0f245c56bb1c18d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eda4de1468cf88f3094f9585bb8bffb",
+    "url": "/sarapage/index.html"
+  },
+  {
+    "revision": "a696ddb759ebdec1cee5",
+    "url": "/sarapage/static/js/2.03d2b668.chunk.js"
+  },
+  {
+    "revision": "dfef45c58f4fcf6c1c1660b909670eb2",
+    "url": "/sarapage/static/js/2.03d2b668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6d3ad9194dcde99e94",
+    "url": "/sarapage/static/js/main.e25a06da.chunk.js"
+  },
+  {
+    "revision": "bf32f1161b160da50a6b",
+    "url": "/sarapage/static/js/runtime-main.bdc2462b.js"
+  },
+  {
+    "revision": "09bcf13bd4647b8ec1791851a9ef6113",
+    "url": "/sarapage/static/media/sara.09bcf13b.png"
+  },
+  {
+    "revision": "553eb8eeddcaeb1eb3d6891cc27eeebc",
+    "url": "/sarapage/static/media/tarot1.553eb8ee.png"
+  },
+  {
+    "revision": "d3ac6a9c4a7168f7da3a1cf3f0267270",
+    "url": "/sarapage/static/media/tarot2.d3ac6a9c.png"
+  },
+  {
+    "revision": "55ff17eb000d244b9e1b559c1046c4e3",
+    "url": "/sarapage/static/media/tarot3.55ff17eb.png"
+  }
+]);
