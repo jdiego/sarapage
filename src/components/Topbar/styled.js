f5c91e53dba9d2a4d9135fa805b23772d8ec392e
Diego Saraiva <diegosaraiva@gmai.com>
2020-09-29 14:14:15 -0300

    adding light style

--- src/components/Topbar/styled.js
+++ src/components/Topbar/styled.js
@@ -1,27 +1,31 @@
 import styled from 'styled-components';
 import { Navbar, NavItem, NavbarBrand, NavLink } from '@bootstrap-styled/v4';
 
+
 export const TopBar = styled(Navbar)` 
     background-color: #858CBC;
-    color: white;
+    color: white !important;
     padding: 0;
     margin: 0;
 `
 export const TopBrand = styled(NavbarBrand)` 
     font-family: "Ubuntu";
     font-size: 2.5rem;
-    font-weight: bold;
+    font-weight: 600;
+    color: white !important;
 `
 
 export const TopItem = styled(NavItem)` 
     padding: 0 18px;
+    display: inline;
+    position: relative;
+    overflow: hidden;
+    
 `
     
 
 export const TopLink = styled(NavLink)` 
     font-size: 1.2rem;
-    font-weight: lighter;
-    color: white ;
-    padding: 10px 15px;
-    
+    font-weight: 600;
+    color: white !important;
 `
